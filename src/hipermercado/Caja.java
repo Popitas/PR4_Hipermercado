9503c2c6f551cb54218e806c65a50b09f0ceb2de
Jorge <jjb5@hotmail.es>
2015-04-18 21:23:08 +0100

    Contabilidad

--- src/hipermercado/Caja.java
+++ src/hipermercado/Caja.java
@@ -1,5 +1,27 @@
 package hipermercado;
 
 public class Caja extends Thread {
+    private Cola<Cliente> queue;
+    private Contabilidad accounting;
 
+    public Caja(Cola<Cliente> queue, Contabilidad accounting) {
+        this.queue = queue;
+        this.accounting = accounting;
+    }
+
+    public void run() {
+        Cliente cliente = queue.attend();
+
+        if (cliente == null) return;
+
+        try {
+            sleep(getAttentionMillis(cliente));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private long getAttentionMillis(Cliente cliente) {
+        return (long)cliente.damePrecioCarro()*10;
+    }
 }
